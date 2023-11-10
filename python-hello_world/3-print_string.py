f57a516517a8f2246ef201909aa80e412bd7daae
Frandy G Slueue <frandy4ever@gmail.com>
2023-11-10 17:22:40 -0600

    add 3-print file

---
+++ python-hello_world/3-print_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""
+Print 3 times a string stored in the variable str, followed by its 
+first 9 characters.
+"""
+str = "Holberton School"
+print("{}".format(str * 3))
+print("{}".format(str[:9]))
