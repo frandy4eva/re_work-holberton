70eb98fbf319f0741ec89cbfff4a2763621cfe83
Frandy G Slueue <frandy4ever@gmail.com>
2023-11-10 17:46:43 -0600

    add 6-concat file

---
+++ python-hello_world/6-concat_edges.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Print `object-oriented programming with Python`"""
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+print("{}".format(str[39:66]), end=' ')
+print("{}".format(str[-22:-18]), end=' ')
+print("{}".format(str[:6]))
