f01c9056d2a71e0154c3e351990b047d98b70491
Frandy G Slueue <frandy4ever@gmail.com>
2023-11-10 17:33:10 -0600

    add 5-edges file

---
+++ python-hello_world/5-edges.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""Usage of slice in python"""
+word = "Holberton"
+print(f"First 3 letters: {word[:3]}")
+print(f"Last 2 letters: {word[-2:]}")
+print(f"Middle word: {word[1:-1]}")
