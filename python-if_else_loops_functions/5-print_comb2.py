a83d3873c8c26c571ad5a6367c9d039daedb3d0f
Frandy G Slueue <frandy4ever@gmail.com>
2023-11-10 23:47:24 -0600

    add 5-print_comb2 file

---
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,7 @@
+
+"""Prints numbers from 0 to 99 with two digits separated by comma except 99"""
+for number in range(100):
+    if number == 99:
+        print(f"{number}\n", end='')
+    else:
+        print(f"{number:02d}", end=", ")
