a23341c4388f470a2e7d5d09dc43d73d01140032
Frandy G Slueue <frandy4ever@gmail.com>
2023-11-10 22:51:34 -0600

    add 0-positive_or_negative file

---
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,10 @@
+
+"""Assign random number to variable number"""
+from random import randint
+number = randint(-10, 10)
+if number > 0:
+    print("{} is positive".format(number))
+elif number == 0:
+    print("{} is zero".format(number))
+else:
+    print("{} is negative".format(number))
