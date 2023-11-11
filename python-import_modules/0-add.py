302e66ae9ba032ed0706355238e52702dd5f5e1c
Frandy G Slueue <frandy4ever@gmail.com>
2023-11-11 10:00:22 -0600

    add 0-add file

---
+++ python-import_modules/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Program that imports the function def add(a, b): from the file add_0.py"""
+from add_0 import add
+
+if __name__ == '__main__':
+    a = 1
+    b = 2
+
+    print(add(a, b))
