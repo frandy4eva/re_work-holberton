14f01d6e70bc7b6b0ea73b3efcdfa2eb6b842119
Frandy G Slueue <frandy4ever@gmail.com>
2023-11-17 08:53:32 -0600

    add 100-my_cal file

---
+++ python-import_modules/100-my_calculator.py
@@ -0,0 +1,15 @@
+from calculator_1 import add, sub, mul, div
+import sys
+
+if __name__ == '__main__':
+    operators = {'+': add, '-': sub, '*': mul, '/': div}
+    if len(sys.argv) - 1 != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+    if sys.argv[2] not in list(operators.keys()):
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+    a = int(sys.argv[1])
+    operator = sys.argv[2]
+    b = int(sys.argv[3])
+    print("{} {} {} = {}".format(a, operator, b, operators[operator](a, b)))
