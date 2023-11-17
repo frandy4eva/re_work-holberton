3ab5a25fb88cbfa782919546a79ffe76bf51b8b6
Frandy G Slueue <frandy4ever@gmail.com>
2023-11-16 20:20:50 -0600

    add 1-calculator.py file

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,13 +1,14 @@
 
 """Program that imports functions from the file calculator_1.py, does some Maths, and prints the result."""
 
-from calculator_1 import add, sub, mul, div
+from test_dir.calculator_1 import add, sub, mul, div, mod
 
 if __name__ == '__main__':
-    a = 10
-    b = 5
+    a = 15
+    b = 2
 
     print(f"{a} + {b} = {add(a, b)}")
     print(f"{a} - {b} = {sub(a, b)}")
     print(f"{a} * {b} = {mul(a, b)}")
-    print(f"{a} / {b} = {div(a, b)}")
+    print(f"{a} / {b} = {div(a, b):.2f}")
+    print(f"{a} % {b} = {div(a, b):.2f}")
