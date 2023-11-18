e5bce7c6a8cfd60f9dd06c0629ce24d6aac9cd36
Frandy G Slueue <frandy4ever@gmail.com>
2023-11-17 19:59:43 -0600

    add 2-replace_in_list

---
+++ python-data_structures/2-replace_in_list.py
@@ -0,0 +1,19 @@
+"""Replace an element of a list at a specific position."""
+
+
+def replace_in_list(my_list, idx, element):
+    """Replace the element in my_list at the specified index with a new value.
+
+    Args:
+    - my_list (list): The list containing values.
+    - idx (int): The index at which the element is to be replaced.
+    - element: The new value to be inserted.
+
+    Returns:
+    list: The modified list after the replacement.
+    list: The original list if the given index is out of bounds or negative.
+    """
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    my_list[idx] = element
+    return my_list
