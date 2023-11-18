09b2cd8ffed471ac99780242e8ca53419da809a6
Frandy G Slueue <frandy4ever@gmail.com>
2023-11-18 16:53:42 -0600

    improve on 8-multiple_returns code

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -2,6 +2,7 @@
 
 
 def multiple_returns(sentence):
-    if len(sentence) == 0:
-        return (0, None)
-    return len(sentence), sentence[0]
+    try:
+        return len(sentence), sentence[0]
+    except IndexError:
+        return 0, None
