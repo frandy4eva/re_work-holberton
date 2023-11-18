63fe5f6da7cfccdd5d5e131cbb9beb5661aab163
Frandy G Slueue <frandy4ever@gmail.com>
2023-11-18 16:48:32 -0600

    improve on 7-add_tuple file code

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -2,15 +2,11 @@
 
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) < 2:
-        if len(tuple_a) == 0:
-            tuple_a = 0, 0
-        else:
-            tuple_a = tuple_a[0], 0
+    # Ensure the tuples have at least two elements
+    tuple_a = (tuple_a[0] if len(tuple_a) > 0 else 0,
+               tuple_a[1] if len(tuple_a) > 1 else 0)
+    tuple_b = (tuple_b[0] if len(tuple_b) > 0 else 0,
+               tuple_b[1] if len(tuple_b) > 1 else 0)
 
-    if len(tuple_b) < 2:
-        if len(tuple_b) == 0:
-            tuple_b = 0, 0
-        else:
-            tuple_b = tuple_a[0], 0
+    # Return a tuple with the sum of corresponding elements
     return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
