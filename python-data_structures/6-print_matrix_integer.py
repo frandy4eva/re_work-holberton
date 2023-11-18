e58b407bc805eb52ce4ef6b865a7f9267049601d
Frandy G Slueue <frandy4ever@gmail.com>
2023-11-18 16:35:07 -0600

    improved 6-print_matrix code

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,9 +2,7 @@
 
 
 def print_matrix_integer(matrix=[[]]):
-    for row in range(len(matrix)):
-        for col in range(len(matrix[row])):
-            print("{}".format(matrix[row][col]), end=' ')
-            if col != (len(matrix[row]) - 1):
-                print(' ', end='')
+    for row in matrix:
+        for elem in row:
+            print("{}".format(elem), end=' ')
         print('')
